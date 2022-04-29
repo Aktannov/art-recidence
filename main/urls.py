afd42aef2cb111242b85464694e615808a311fcb
chethahernya <povvezzlo@gmail.com>
2022-04-29 18:55:49 +0600

    first commit

---
+++ main/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.view),
+    path('info', views.info),
+    path('inffo', views.inffo),
+    path('test', views.test),
+    path('vet', views.vet),
+    path('kart', views.kart),
+    path('kino', views.kino),
+    path('pict', views.pict),
+    path('disc', views.disc),
+    path('chedo', views.chedo),
+    path('stoi', views.stoi),
+    path('velo', views.velo),
+    path('leo', views.leo),
+    path('kmn', views.kmn),
+    path('ben', views.ben),
+    path('lev', views.lev),
+    path('tay', views.tay),
+    path('kmr', views.kmr),
+    path('dvr', views.dvr),
+    path('kvr', views.kvr),
+    path('selk', views.selkin),
+    path('bat', views.batink),
+]
---
+++ main/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view(request):
+    return render(request, 'main/main.html')
+
+def inffo(request):
+    return render(request, 'main/inffo.html') 
+
+def info(request):
+    return render(request, 'main/hu.html')
+
+def test(request):
+    return render(request, 'main/test.html')
+
+def vet(request):
+    return render(request, 'main/vet.html')
+
+def kart(request):
+    return render(request, 'main/kart.html')
+
+def kino(request):
+    return render(request, 'main/kino.html')
+
+def pict(request):
+    return render(request, 'main/pict.html')
+
+def disc(request):
+    return render(request, 'main/disc.html')
+
+def chedo(request):
+    return render(request, 'main/chedo.html')
+
+def stoi(request):
+    return render(request, 'main/stoi.html')
+
+def velo(request):
+    return render(request, 'main/velo.html')
+
+def leo(request):
+    return render(request, 'main/leo.html')
+
+def kmn(request):
+    return render(request, 'main/kmn.html')
+
+def ben(request):
+    return render(request, 'main/ben.html')
+
+def lev(request):
+    return render(request, 'main/lev.html')
+
+def tay(request):
+    return render(request, 'main/tay.html')
+
+def kmr(request):
+    return render(request, 'main/kmr.html')
+
+def dvr(request):
+    return render(request, 'main/dvr.html')
+
+def kvr(request):
+    return render(request, 'main/kvr.html')
+
+def selkin(request):
+    return render(request, 'main/selkin.html')
+
+def batink(request):
+    return render(request, 'main/batinok.html') 
