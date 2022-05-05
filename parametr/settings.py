bcfd88ed5decefb52cc94100405ad74ff3eb06f9
chethahernya <povvezzlo@gmail.com>
2022-05-05 23:17:28 +0600

    финальные поправки

--- parametr/settings.py
+++ parametr/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'parametr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
     }
