f82ed053c990376ec2473924a7a1cb30d85e7796
chethahernya <povvezzlo@gmail.com>
2022-05-05 21:29:30 +0600

    for site

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def view(request):
-    return render(request, 'main/main.html')
+    return render(request, 'main/index.html')
 
 def inffo(request):
     return render(request, 'main/inffo.html') 
--- parametr/settings.py
+++ parametr/settings.py
@@ -12,6 +12,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*if1@7t_)h@&2k_=)ik1bhrfu6i!9%3^9xwoxu*tuzbk$b_cj+'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -76,10 +78,10 @@ WSGI_APPLICATION = 'parametr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kuntuu',
-        'USER': 'aktai',
-        'PASSWORD': '1',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
 
